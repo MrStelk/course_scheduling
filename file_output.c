3e673a409ac0815cc107208f3d52fa03ce86c238
karthik-NvY <karthikndasaraju@gmail.com>
2022-11-07 22:34:48 +0530

    seperated from main

--- file_output.c
+++ file_output.c
@@ -1,18 +1,25 @@
-#include<stdio.h>
-int main(){
-int i;
-    FILE *file;
-    file = fopen(".\\Courses Schedule.txt","w");
-    fprintf(file,"    COURSES SCHEDULE\n\n");
-    for(i=0;i<no_of_batches;i++)
-    {
-        fprintf(file,"%s \n",batch_names[i]);
-        for(int j =0;j<batch_data[i]->length;j++)
-        {
-            int n=batch_data[i]->arr[j];
-            fprintf(file,"%s  -  %s\n",course_names[n], sessions[ courses[n].color]);
-        }
-        fprintf(file,"\n\n\n");
-    }
+extern new_arr** batch_data;
+extern course_node* courses;
 
+extern int no_of_branches;
+extern int no_of_courses;
+extern int no_of_colors;
+
+extern char** course_names;
+extern char** batch_names;
+
+void outfile(new_arr** batch_data)
+{
+	FILE* file = fopen("Schedule.txt","w");
+	fprintf(file,"    COURSES SCHEDULE\n\n");
+	for(int i=0;i<no_of_batches;i++)
+	{
+		fprintf(file,"%s \n",batch_names[i]);
+		for(int j =0;j<batch_data[i]->length;j++)
+		{
+			int n=batch_data[i]->arr[j];
+			fprintf(file,"%s  -  %d\n",course_names[n], courses[n].color);
+        	}
+		fprintf(file,"\n\n\n");
+	}
 }
