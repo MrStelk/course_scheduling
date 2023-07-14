fe13ba70d36fb41e43967c62685e2db7858ac6b9
karthik-NvY <karthikndasaraju@gmail.com>
2023-07-14 17:43:57 +0530

    conflict resolved

--- main.c
+++ main.c
@@ -1,107 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "data_types.h"
+#include "graph_init.c"
+#include "heap.c"
+#include "file_parser.c"
 
-#define no_of_batches 4
-#define no_of_colors 5
-#define no_of_courses 8
-
-
-// Array with a length attribute.
-typedef struct{
-	int length;
-	int* arr;
-}new_arr;
+int no_of_batches;
+int no_of_colors;
+int no_of_courses;
 
 
-
-// We'll create a hashmap from course_index to name of course.
-typedef struct{
-	int course_index;
-	int colored;
-	int color;
-	int edges;
-	new_arr neighbours;
-}course_node;
+char** course_names;
+char** batch_names;
+char** color_names;
 
 
-// Heaping will happen on edges.
-typedef struct{
-	int course_index;
-	int edges;
-}heap_node;
 
 course_node* courses; // Courses should be an array of course_nodes.
 new_arr** batch_data; // Array of batches and their courses.
 heap_node* heap; // heap
 
-int heap_size = no_of_courses;
 
-int** make_graph(); creates graph.
-void Build_heap(heap_node* heap);
-heap_node Extract_max(heap_node* heap);
-void Max_heapify(heap_node* heap, int idx);
+int backtrack();
+int check_color(int course_index , int color);
 
-
-void update_course_nodes(int** graph, course_node* courses);
-
+int colors[4] = {0, 1, 2, 3};
+int heap_size = 8;
 
 int main(void)
 {
+	FILE* file = fopen("inp.txt", "r");
+	char strin[30];
+	fgets(strin, 30, file);
+
+	strin[strleng(strin)-1] = '\0';
+	no_of_batches = strtoint(&strin[16]);
+
 	batch_data = (new_arr**)malloc(sizeof(new_arr*)*no_of_batches);
+	parsefile(file, batch_data);
+	
 	heap = (heap_node*)malloc(sizeof(heap_node)*no_of_courses);
 	courses = (course_node*)malloc(sizeof(course_node)*no_of_courses);
 
-	batch_data[0] = (new_arr*)malloc(sizeof(new_arr));
-	batch_data[1] = (new_arr*)malloc(sizeof(new_arr));
-	batch_data[2] = (new_arr*)malloc(sizeof(new_arr));
-	batch_data[3] = (new_arr*)malloc(sizeof(new_arr));
-	
-	batch_data[0]->length = 4;
-	batch_data[1]->length = 2;
-	batch_data[2]->length = 3;
-	batch_data[3]->length = 4;
-
-	batch_data[0]->arr = (int*)malloc(sizeof(int)*4);
-	batch_data[1]->arr = (int*)malloc(sizeof(int)*2);
-	batch_data[2]->arr = (int*)malloc(sizeof(int)*6);
-	batch_data[3]->arr = (int*)malloc(sizeof(int)*3);	
-
-	batch_data[0]->arr[0] = 2;
-	batch_data[0]->arr[1] = 4;
-	batch_data[0]->arr[2] = 7;
-	batch_data[0]->arr[3] = 6;
-
-	batch_data[1]->arr[0] = 3;
-	batch_data[1]->arr[1] = 1;
-
-	batch_data[2]->arr[0] = 5;
-	batch_data[2]->arr[1] = 0;
-	batch_data[2]->arr[2] = 2;
-
-
-	batch_data[3]->arr[0] = 1;
-	batch_data[3]->arr[1] = 6;
-	batch_data[3]->arr[2] = 0;
-	batch_data[3]->arr[3] = 4;
-	
 	int** p = make_graph();
 
-	update_course_nodes(p, courses); // Todo
-
-	build_heap(heap);
+	update_course_nodes(p, courses);
+	Build_heap(heap);
 
 	int result = backtrack();
 	if(result)
 	{
-		print_data(); // Todo.
+		for(int i=0; i<no_of_courses; i++) // Todo.
+		{
+			printf("%d -- %d\n", i,courses[i].color);
+		}
 	}
 	else
 	{
-		print_no_solution(); // Todo.
+		printf("No solution"); // Todo.
 	}
-		
-	clear_memory(); // Todo
-	return;
+		 // Todo - clear memory.
+	return 0;
 }
 
 
@@ -112,10 +72,10 @@ int backtrack()
 		heap_node course = Extract_max(heap);
 		for(int i=0; i<no_of_colors; i++)
 		{
-			if(check_color(course.course_idx, colors[i])
+			if(check_color(course.course_index, colors[i]))
 			{	
-				courses[course_index].color = colors[i];
-				courses[course_index].colored = 1;
+				courses[course.course_index].color = colors[i];
+				courses[course.course_index].colored = 1;
 		
 				if(!backtrack())
 				{
@@ -131,110 +91,16 @@ int backtrack()
 
 
 
-void Build_heap(heap_node* heap)
+// Checks if color can be assigned.
+int check_color(int course_index , int color)
 {
-	for(int i=0; i<no_of_courses; i++)
-	{	
-		heap[i].course_index = i;
-		heap[i].edges = courses[i].edges;
-	}
-	for(int i=no_of_courses/2; i>=0; i--)
-	{	
-		Max_heapify(heap, i);
-	}
-}
-
-
-void Max_heapify(heap_node* heap, int idx)
-{
-	int left = idx*2+1;
-	int right = (idx+1)*2;
-	int largest = idx;
-
-	if((left < heap_size) && (heap[left].edges > heap[largest].edges))
+	for(int i = 0 ; i < courses[course_index].neighbours.length; i++)
 	{
-		largest = left;
-	}
-
-	if((right < heap_size) && (heap[right].edges > heap[largest].edges))
-	{
-		largest = right;
-	}
-	
-	if(largest != idx)
-	{
-		heap_node tmp = heap[idx];
-		heap[idx] = heap[largest];
-		heap[largest] = tmp;
-		
-		Max_heapify(heap, largest);
-	}
-	return;
-}
-
-heap_node Extract_max(heap_node* heap)
-{
-	heap_node result =  heap[0];
-	heap_size--;
-	heap[0] = heap[heap_size];
-	
-	Max_heapify(heap, 0);
-	return result;
-}
-
-
-int** make_graph() 
-{
-	int** graph = malloc(sizeof(int*)*no_of_courses);
-	for(int i=0; i<no_of_courses; i++)
-	{
-		graph[i] = malloc(sizeof(int)*no_of_courses);
-	}
-	
-	for(int i=0; i<no_of_courses; i++)
-	{
-		for(int j=0; j<no_of_courses; j++)
+		if((courses[courses[course_index].neighbours.arr[i]].colored == 1) && (courses[courses[course_index].neighbours.arr[i]].color == color))
 		{
-			graph[i][j] = 0;
+			return 0; //returns 0 if neighbor course color matches with the given color(i.e we should not give this color to the given course).
 		}
 	}
-	
-	for(int i=0; i<no_of_batches; i++)
-	{
-		for(int j=0; j<batch_data[i]->length; j++)
-		{
-			for(int k=0; k<batch_data[i]->length; k++)
-			{
-				if(j != k)
-				{
-					graph[batch_data[i]->arr[j]][batch_data[i]->arr[k]] = 1;
-				}
-			}
-		}
-	}
-
-	return graph;
+	return 1; //return 1 indicates we can give the color to the course.
 }
 
-// Updates no of edges for all courses.
-void update_course_nodes(int** graph, course_node* courses) // Graph will be available globally.
-{
-	int i,k;
-	for(k=0;k<no_of_courses;k++)
-	{
-		courses[k].edges = 0;
-		courses[k].neighbours.length = 0;
-		courses[k].neighbours.arr = (int*)malloc(sizeof(int));
-	
-		for(i=0;i<no_of_courses;i++)
-		{
-	        	if(graph[k][i])
-         		{
-              			courses[k].edges++;
-				courses[k].neighbours.length++;
-				courses[k].neighbours.arr = (int*)realloc(courses[k].neighbours.arr, sizeof(int)*courses[k].neighbours.length);
-				courses[k].neighbours.arr[(courses[k].neighbours.length)-1] = i;
-			}
-      		}
-      }
-}
